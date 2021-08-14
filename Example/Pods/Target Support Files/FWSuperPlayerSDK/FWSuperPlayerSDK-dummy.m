1f2233814e6f3338efa505658a32265c4682da85
SailorGa <ljia789@gmail.com>
2021-08-14 14:53:46 +0800

    补充提交

--- Example/Pods/Target Support Files/FWSuperPlayerSDK/FWSuperPlayerSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FWSuperPlayerSDK : NSObject
-@end
-@implementation PodsDummy_FWSuperPlayerSDK
-@end
--- Example/Pods/Target Support Files/Pods-FWSuperPlayerSDK_Example/Pods-FWSuperPlayerSDK_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FWSuperPlayerSDK_Example : NSObject
-@end
-@implementation PodsDummy_Pods_FWSuperPlayerSDK_Example
-@end
